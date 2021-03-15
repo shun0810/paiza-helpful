dc5195d4974db544bfc5d638418cdd3fc46dd4bd
shun0810 <shun.matsumoto@asia-quest.jp>
2021-03-15 15:12:05 +0900

    commit

---
+++ app.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+require_once 'vendor/autoload.php';
+
+use shun0810\PaizaHelpful\D166;
+
+$app = new D166();
+$app->main();
---
+++ src/D166.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace shun0810\PaizaHelpful;
+
+class D166
+{
+    public function main(): void
+    {
+        echo 'OK', PHP_EOL;
+    }
+}
